cada8bed6485a02521b5a310170bff91af55062a
operaxxx <a3303603@live.com>
2025-01-15 17:58:26 +0800

    Initialize Vue 3 project with Vite, Ant Design, and Pinia; add essential configuration files, components, and basic layout structure.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BasicLayout from '@/components/layout/BasicLayout.vue'
+</script>
+
+<template>
+  <BasicLayout>
+    <RouterView />
+  </BasicLayout>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+import 'ant-design-vue/dist/reset.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import AntDesignVue from 'ant-design-vue'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(AntDesignVue)
+app.use(router)
+app.mount('#app')
